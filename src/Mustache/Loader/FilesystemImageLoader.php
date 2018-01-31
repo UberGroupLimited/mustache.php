87503a6bb0581d703f6179131be3fdf99df5d846
Warren <warren.r.strong@gmail.com>
2018-02-01 10:03:12 +1300

    Update FilesystemImageLoader.php

--- src/Mustache/Loader/FilesystemImageLoader.php
+++ src/Mustache/Loader/FilesystemImageLoader.php
@@ -3,28 +3,28 @@
 
 class Mustache_Loader_FilesystemImageLoader extends Mustache_Loader_FilesystemLoader implements Mustache_Loader
 {
-	
+
 	var $map = array(
 		'png' => 'png',
 		'jpg' =>'jpeg',
+		'ttf' =>'ttf',
 	);
-
-	function __construct($baseDir, array $options = array())
-	{
-		parent::__construct($baseDir, ($options));
-	}
 
 
 	public function load($name)
 	{
+
 		$type = substr($name, (strrpos($name, '-', -4)+1));
 
 		if(!isset($this->map[$type]))
 			throw new Mustache_Exception_InvalidArgumentException('Image type '.$type.' does not appear to be valid');
 
-		$x = parent::load($name);
+		$bfr = parent::load($name);
 
-		return 'data:image/'.$this->map[$type].';base64,'.base64_encode($x);
+		$mime = new finfo(FILEINFO_MIME);
+
+		return 'data:'.$mime->buffer($x).';base64,'.base64_encode($bfr);
+
 	}
 
 }
